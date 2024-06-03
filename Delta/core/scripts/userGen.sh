0607cebf223f6ac897ae9e8cb8396deee9b09693
PhoenixDragon623 <64208901+phoenixcentipede623@users.noreply.github.com>
2024-06-04 03:36:26 +0530

    Update userGen.sh

--- Delta/core/scripts/userGen.sh
+++ Delta/core/scripts/userGen.sh
@@ -3,7 +3,7 @@
 deregistrationChech_path= PATH
 displayStatus_path= Path
 
-core_home="/home/core"
+core="/home/core"
 mentors_home="$core_home/mentors"
 mentees_home="$core_home/mentees"
 domains=("Webdev" "Appdev" "Sysad")
@@ -19,10 +19,7 @@ sudo mkdir -p /home/$core/mentors /home/$core/mentees
 sudo chmod 755 "$core_home"
 
 
-
-
-
-for domain in "{domains[@]}"; do
+for domain in "${domains[@]}"; do
     sudo mkdir -p "$mentors_home/$domain"
 done
 
@@ -30,53 +27,28 @@ done
 while IFS=' ' read -r name domain capacity; do
     mentor="$domain"_"$name"
     if ! id -u "$mentor" &>/dev/null; then
-        sudo useradd -m "$mentor"
+        sudo useradd -m -d /home/core/mentors/$domain "$mentor"
     fi
     # we redefine mentor_home to this specific mentor
-    local mentor_home="$mentees_home/$domain/$mentor"
-    sudo mkdir -p "$mentors_home"
-    sudo mv "/home/$mentor" "$mentors_home"
+    local mentor_home="/home/core/mentors/$domain/$mentor"
     sudo touch "$mentors_home/allocatedMentees.txt"
-
     for i in {1..3}; do
-        sudo mkdir -p $mentors_home/submittedTasks/task$i
+        sudo mkdir -p "$mentors_home/submittedTasks/task$i"
     done
+done < "$core/mentorDetails.txt"
 
-done < "mentorDetails.txt"
 
-
-"""
-#Creating mentor users and moving home to right place
-    sudo useradd -m $mentor
-    sudo mkdir /home/$core/mentors/$mentor
-    sudo mv /home/$mentor /home/$core/mentors/$mentor
-
-    #files
-    sudo touch /home/$core/mentors/$mentor/allocatedMentees.txt
-    sudo mkdir /home/$core/mentors/$mentor/submittedTasks
-    sudo mkdir /home/$core/mentors/$mentor/submittedTasks/task1 /home/$core/mentors/$mentor/submittedTasks/task2 /home/$core/mentors/$mentor/submittedTasks/task3
-    '''
-    will this work!!
-    for i in {1..3}; do
-        sudo mkdir /home/$core/domain/$mentor/submittedTasks/task$i
-    '''
-done
-"""
-#Creating mentee users....and adding respectivefiles
+#Creating mentee users....and adding respective files
 while IFS=' ' read -r mentee_roll name domains; do
     
     if ! id -u "$mentee_roll" &>/dev/null; then
-        sudo useradd -m "$mentee_roll"
+        sudo useradd -m -d /home/core/mentees "$mentee_roll"
     fi
     #same here
-    local mentees_home="$mentees_home/$mentee_roll"
-    sudo mkdir -p "$mentees_home"
-    sudo mv "/home/$mentee" "$mentee_home"
-    
-    
-    sudo touch $mentee_home/domain_pref.txt
-    sudo touch $mentee_home/task_submitted.txt
-    sudo touch $mentee_home/task_completed.txt
+    local mentees_home="/home/core/mentees/$mentee_roll"
+    sudo touch "$mentee_home/domain_pref.txt"
+    sudo touch "$mentee_home/task_submitted.txt"
+    sudo touch "$mentee_home/task_completed.txt"
 
 
     IFS="->" read -r -a domain_pref <<< "$domains"
@@ -86,7 +58,7 @@ while IFS=' ' read -r mentee_roll name domains; do
             sudo mkdir -p $mentee_home/$domain/task$i
         done
     done
-done < "mentees_domain.txt"
+done < "$core/mentees_domain.txt"
 
 # Permissions
  # $mentor home wont create issues as variable are only defined within the scope
@@ -109,7 +81,7 @@ sudo chmod 644 "/home/$core/mentorDetails.txt"
 # permissions
 
 sudo chmod 755 /home/$core
-sudo chmod 700 /home/$core/mentors/* /home/$core/mentees/*
+sudo chmod 700 -r /home/$core/mentors /home/$core/mentees
 
 sudo chmod 777 /home/$core/mentees
 
